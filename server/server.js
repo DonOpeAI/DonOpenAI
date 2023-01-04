ed0f18e4c2f899e5411006c924ed08871e65c02a
DonOpeAI <maxsmithh33@gmail.com>
2023-01-04 14:49:10 -0800

    update the message

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Thanks Don For Developing Me'
+    message: 'Thanks Don For Developing Me.'
   })
 })
 
